8d1dda17ea948a497785f85665fc700e88cd024a
Verthae <88465477+Verthae@users.noreply.github.com>
2022-10-15 14:10:08 -0400

    add raw path

--- lib/paths.py
+++ lib/paths.py
@@ -2,3 +2,4 @@ import os
 #  Modify ROOT to whatever directory you want to store in - default is the same folder that holds lib
 ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 ASSETS = os.path.join(ROOT, "assets")
+RAW = os.path.join(ROOT, "raw")
